7347d2a5590b4162163705260ca3e3e5b475b068
satya2221 <satyaghifari12@gmail.com>
2021-01-27 12:27:30 +0700

    SVM selesai plus grafiknya sudah jadi

--- sentiment_analysis_SVM.R
+++ sentiment_analysis_SVM.R
@@ -6,60 +6,70 @@ library(tidyverse)
 library(RTextTools)
 library(e1071)
 
+svm <- function (tweet_all, positive, negative, length_data) {
+  #buaat factor ada berapa banyak positive dan negativenya
+  sentiment <- c(rep("positive", nrow(positive)), rep("negative", nrow(negative)))
+  sentiment <- as.factor(sentiment)
+
+  # menyatukan kalimat positif dan negatif jadi satu variabel
+  dataDetail <- full_join(positive,negative)
+  dataDetail <- as_tibble(dataDetail)
+
+  # buat twitter jadi tibble biar bisa disatukan dengan dataDetail
+  tweet_all <- as_tibble(tweet_all)
+
+  # buat nama kolom value di dataDetail jadi text
+  #colnames(dataDetail)[colnames(dataDetail)=="value"] <- "text"
+
+  # gabungkan detailData dengan tweet menjadi dataframe
+  df <- full_join(dataDetail,tweet_all)
+
+  # buat matrix dari dataframe
+  mat <- create_matrix(df, language = "english", removeStopwords = TRUE,
+                     stemWords = TRUE, tm::weightTfIdf)
+  mat <- as.matrix(mat)
+
+  length_parameter <- 2000 + length_data
+  # buat container dari matrix. disini data sentimen dengan tweet akan dipisahkan lagi
+  container <- create_container(mat, as.numeric(sentiment), trainSize = 1:2000,
+   testSize = 2001:length_parameter, virgin = TRUE)
+
+  # latih modelnya dengan algoritma SVM
+  models <- train_models(container, algorithms = "SVM")
+
+  # prediksi tweet nya dan hasilkan
+  results <- classify_models(container, models)
+
+  results <- results %>% mutate(sentiment_nya = if_else(SVM_PROB >= 0.50 & SVM_PROB <= 0.51,"Neutral", if_else(SVM_LABEL==1,"Negative","Positive")))
+
+  results <- results %>% mutate(tweet = tweet_all$value)
+
+  results
+}
+
 set.seed(1234)
-tweet_all <- read.csv("amd_cleaned.csv")
+# panggil dataset kalimat positve negative yang sudah dibersihkan
 positive <- read.csv("E:\\OneDrive\\SMT5\\Datsci\\proyekAkhir\\positive_negative_sentence\\positive_cleaned.csv")
 negative <- read.csv("E:\\OneDrive\\SMT5\\Datsci\\proyekAkhir\\positive_negative_sentence\\negative_cleaned.csv")
 
-#buaat factor ada berapa banyak positive dan negativenya
-sentiment <- c(rep("positive", nrow(positive)), rep("negative", nrow(negative)))
-sentiment <- as.factor(sentiment)
+# tentukan sentimen AMD
+tweet_amd <- read.csv("amd_cleaned_baru.csv")
+amd_result <- svm(tweet_amd,positive, negative, nrow(tweet_amd))
+amd <- ggplot(data = amd_result, aes(x=sentiment_nya, fill = sentiment_nya)) +
+            geom_bar() + labs(title = "AMD")
 
-# menyatukan kalimat positif dan negatif jadi satu variabel
-sentiment_all <- full_join(positive,negative)
-sentiment_all <- as_tibble(sentiment_all)
+# tentukan sentimen NVDA
+tweet_nvda <- read.csv("nvda_cleaned_baru.csv")
+nvda_result <- svm(tweet_nvda,positive, negative,nrow(tweet_nvda))
+nvda<- ggplot(data = nvda_result, aes(x=sentiment_nya, fill = sentiment_nya)) +
+            geom_bar() + labs(title = "NVDA")
 
-tweet_all <- as_tibble(tweet_all)
+# tentukan sentimen GOOGL
+tweet_googl <- read.csv("googles_cleaned.csv")
+googl_result <- svm(tweet_googl,positive, negative,nrow(tweet_googl))
+google <- ggplot(data = googl_result, aes(x=sentiment_nya, fill = sentiment_nya)) +
+            geom_bar()+ labs(title = "GOOGL")
 
-colnames(sentiment_all)[colnames(sentiment_all)=="value"] <- "text"
-
-df <- full_join(sentiment_all,tweet_all)
-
-df
-
-#mat_x <- create_matrix(tweet_all, language = "english", removeStopwords = TRUE,
-#                     stemWords = FALSE, tm::weightTfIdf)
-#mat_x <- as.matrix(mat_x)
-
-mat <- create_matrix(df, language = "english", removeStopwords = TRUE,
-                     stemWords = TRUE, tm::weightTfIdf)
-class(mat)
-mat <- as.matrix(mat)
-
-#mat_training <- create_matrix(sentiment_all, language = "english", removeStopwords = TRUE,
-#                     stemWords = FALSE, tm::weightTfIdf)
-#mat_training <- as.matrix(mat_training)
-#
-#gabungan <- merge(mat, mat_training,  by = "row.names", all = TRUE)
-#
-container <- create_container(mat, as.numeric(sentiment), trainSize = 1:2000,
-   testSize = 2001:2511, virgin = TRUE)
-
-models <- train_models(container, algorithms = "SVM")
-#models <- train_models(container, algorithms = "SVM")
-results <- classify_models(container, models)
-# Cross Validation
-
-analytics <- create_analytics(container, results)
-summary(analytics)
-##classifier <- naiveBayes(mat_training, sentiment)
-##predicted <- predict(classifier, mat)
-##predicted
-#
-#tweet_all <- as_tibble(tweet_all)
-#tweet_all <- tweet_all %>% mutate(sentimen_nya = predicted)
-#
-results <- results %>% mutate(sentiment_nya = if_else(SVM_LABEL==1,"Negative","Positive"))
-plotnya <- ggplot(data = results, aes(x=sentiment_nya, fill = sentiment_nya)) +
-            geom_bar()
-plotnya
+ggsave("google.png", plot = google)
+ggsave("nvda.png", plot = nvda)
+ggsave("amd.png", plot = amd)
