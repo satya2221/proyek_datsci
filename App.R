72db9d069f490ff5e6aed97cfce3c2c3e79f8da6
args06 <acool90@yahoo.co.id>
2021-01-27 20:12:04 +0700

    Apdet Syaini

---
+++ App.R
@@ -0,0 +1,77 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: args06
+# Created on: 27/01/2021
+
+library(shiny)
+library(here)
+library(vroom)
+library(dplyr)
+library(ggplot2)
+library(plotly)
+
+sentimentAMD <- read.csv("result_amd.csv")
+sentimentNVDA <- read.csv("result_nvda.csv")
+sentimentGOOGL <- read.csv("result_googl.csv")
+stockName <- c("AMD", "Google", "NVIDIA")
+
+ui <- fluidPage(
+  title = "Sentimen Analisis",
+  headerPanel("Sentimen Analisis Emiten Saham"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "stockName",
+                  label = "Stock Name",
+                  choices = stockName,
+                  selected = stockName[1]),
+    ),
+    mainPanel(
+      tabsetPanel(type = "tabs",
+                  tabPanel("Sentiment Plot", plotlyOutput("plot")),
+                  tabPanel("Sentiment Table", dataTableOutput('table')
+                  )
+      )
+    )
+  )
+)
+
+server <- function(input, output) {
+
+  plotting <- reactive({
+    if (input$stockName == "AMD") {
+      selected <- sentimentAMD
+    } else if (input$stockName == "Google") {
+      selected <- sentimentGOOGL
+    } else {
+      selected <- sentimentNVDA
+    }
+
+    selected %>%
+      ggplot(aes(x = sentiment_nya, fill = sentiment_nya)) +
+      geom_bar() +
+      labs(title = input$stockName) +
+      theme_light()
+  })
+
+  output$plot <- renderPlotly({
+    ggplotly(plotting())
+  })
+
+  tabling <- function(stockName) {
+    if (stockName == "AMD") {
+      selected <- sentimentAMD
+    } else if (stockName == "Google") {
+      selected <- sentimentGOOGL
+    } else {
+      selected <- sentimentNVDA
+    }
+    df <- select(selected, tweet, sentiment_nya)
+    df
+  }
+
+  output$table <- renderDataTable({
+    datatable(tabling(input$stockName))
+  })
+}
+
+shinyApp(ui = ui, server = server)
--- shiny.R
+++
@@ -1,5 +0,0 @@
-# Title     : TODO
-# Objective : TODO
-# Created by: args06
-# Created on: 27/01/2021
-
